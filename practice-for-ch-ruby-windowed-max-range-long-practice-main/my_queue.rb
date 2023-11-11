78c331e02d12c7c9f8a7661d96854bbf93ee9461
Levi Sherman <Levi Sherman>
2023-11-11 14:39:41 -0500

    finished

--- practice-for-ch-ruby-windowed-max-range-long-practice-main/my_queue.rb
+++ practice-for-ch-ruby-windowed-max-range-long-practice-main/my_queue.rb
@@ -1,33 +1,173 @@
-class MyQueue 
-    def initialize 
-        @queue = []
-    end    
+class MyQueue
+    def initialize
+        @store = []
+    end
 
-    def peek 
-        @queue.last 
-    end  
-    
-    def size 
-        @queue.size 
-    end    
+    def peek
+        @store.last
+    end
+
+    def size
+        @store.size
+    end
 
     def empty?
-        @queue.size == 0
+        @store.size == 0
     end
-    
-    def enqueue 
-        @queue.unshift 
-    end    
 
-    def dequeue  
-        @queue.pop
+    def enstore
+        @store.unshift
     end
-end    
+
+    def destore
+        @store.pop
+    end
+end
 
 
-class MyStack 
+class MyStack
+    def initialize(store = [])
+        @store = store
+    end
+
+    def peek
+        @store.last
+    end
+
+    def size
+        @store.size
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def pop
+        @store.pop
+    end
+
+    def push(el)
+        @store.push(el)
+    end
+
+end
+
+
+class StackQueue
     def initialize
-        @queue = []
-    end    
+        @in_stack = MyStack.new
+        @out_stack = MyStack.new
+    end
 
-end    
+    def size
+        @in_stack.size + @out_stack.size
+    end
+
+    def empty?
+        @in_stack.empty? && @out_stack.empty?
+    end
+
+    def enqueue(val)
+        @in_stack.push(val)
+    end
+
+    def dequeue
+        queueify if @out_stack.empty?
+        @out_stack.pop
+    end
+
+    def queueify
+        @out_stack.push(@in_stack.pop) until @in_stack.empty?
+    end
+end
+
+class MinMaxStack
+    def initialize
+        @store = MyStack.new
+    end
+
+    def peek
+        @store.peek[:value] unless empty?
+    end
+
+    def size
+        @store.size
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def max
+        @store.peek[:max] unless empty?
+    end
+
+    def min
+        @store.peek[:min] unless empty?
+    end
+
+    def pop
+        @store.pop[:value] unless empty?
+    end
+
+    def push(val)
+        @store.push({
+            min: new_min(val),
+            max: new_max(val),
+            value: val
+        })
+    end
+
+    private
+    def new_min(val)
+        empty? ? val : [min, val].min
+    end
+
+    def new_max(val)
+        empty? ? val : [max, val].max
+    end
+end
+
+
+class MinMaxStackQueue
+    def initialize
+        @in_stack = MinMaxStack.new
+        @out_stack = MinMaxStack.new
+    end
+
+    def size
+        @in_stack.size + @out_stack.size
+    end
+
+    def empty?
+        @in_stack.empty? && @out_stack.empty?
+    end
+
+    def dequeue
+        queueify if @out_stack.empty?
+        @out_stack.pop
+    end
+
+    def enqueue(val)
+        @in_stack.push(val)
+    end
+
+    def max
+        maxes = []
+        maxes << @in_stack.max unless @in_stack.empty?
+        maxes << @out_stack.max unless @out_stack.empty?
+        maxes.max
+    end
+
+    def min
+        mins = []
+        mins << @in_stack.min unless @in_stack.empty?
+        mins << @out_stack.min unless @out_stack.empty?
+        mins.min
+    end
+
+    private
+    def queueify
+        @out_stack.push(@in_stack.pop) until @in_stack.empty?
+    end
+end
--- practice-for-ch-ruby-windowed-max-range-long-practice-main/test.rb
+++ practice-for-ch-ruby-windowed-max-range-long-practice-main/test.rb
@@ -1,22 +1,41 @@
 require 'byebug'
+require_relative 'my_queue'
+# def windowed_max_range(arr, window_size)
+#     current_max_range = 0
+#     (0..arr.length - window_size).each do |i|
+#         window = arr[i...i + window_size]
+#         min = window.min
+#         max = window.max
+#         range = max - min
+#         current_max_range = range  if range > current_max_range
+#     end
+#     return current_max_range
+# end
+
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
 
 
-def windowed_max_range(arr, window_size)  
-    current_max_range = 0  
-    (0..arr.length - window_size).each do |i|
-        window = arr[i...i + window_size]
-        min = window.min 
-        max = window.max  
-        range = max - min 
-        # debugger 
-        current_max_range = range  if range > current_max_range
-    end    
-    return current_max_range
-end    
+def max_windowed_range(array, window_size)
+    queue = MinMaxStackQueue.new
+    best_range = nil
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
+    array.each_with_index do |el, i|
+        queue.enqueue(el)
+        queue.dequeue if queue.size > window_size
 
+        if queue.size == window_size
+            current_range = queue.max - queue.min
+            best_range = current_range if !best_range || current_range > best_range
+        end
+    end
 
+    best_range
+end
+
+p max_windowed_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p max_windowed_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p max_windowed_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p max_windowed_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
